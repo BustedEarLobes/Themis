8bbd849e63c3fc645963658389269ee76519c9e6
Cameron Hessler <cameronhessler123@gmail.com>
2017-08-19 18:20:39 -0500

    Added save/load state feature to taskmanager.

--- src/main/java/com/github/bustedearlobes/themis/taskmanager/TaskManager.java
+++ src/main/java/com/github/bustedearlobes/themis/taskmanager/TaskManager.java
@@ -1,5 +1,14 @@
 package com.github.bustedearlobes.themis.taskmanager;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,10 +19,16 @@ import java.util.logging.Logger;
 
 public class TaskManager implements Runnable {
     private static final Logger LOG = Logger.getLogger("Themis");
-
+    private static final File STATE_FILE = new File("themis_state.dat");
+    
     private List<ScheduledTask> scheduledTasks;
     private AtomicBoolean isRunning;
     private ExecutorService executor = Executors.newCachedThreadPool();
+    
+    public TaskManager() {
+        scheduledTasks = new LinkedList<>();
+        loadOldState();
+    }
     
     public void addTaskToScheduler(ScheduledTask task) {
         synchronized(scheduledTasks) {
@@ -52,6 +67,36 @@ public class TaskManager implements Runnable {
             }
         } catch(InterruptedException e) {
             LOG.log(Level.WARNING, "Could not shut down task manager safely. Interrupted Exception.", e);
+        }
+        saveState();
+    }
+    
+    private void loadOldState() {
+        try(FileInputStream fis = new FileInputStream(STATE_FILE)) {
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            Object obj;
+            int count = 0;
+            while((obj = ois.readObject()) != null) {
+                addTaskToScheduler((ScheduledTask)obj);
+                count ++;
+            }
+            LOG.log(Level.INFO, "Task manager state loaded " + count + " tasks from " + STATE_FILE.getCanonicalPath());
+        } catch(IOException | ClassNotFoundException e) {
+            LOG.log(Level.SEVERE, "Could not load task manager state.", e);
+        }
+    }
+    
+    private void saveState() {
+        try(FileOutputStream fos = new FileOutputStream(STATE_FILE)) {
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            for(ScheduledTask task : scheduledTasks) {
+                oos.writeObject(task);
+            }
+            LOG.log(Level.INFO, "Task manager state saved to " + STATE_FILE.getCanonicalPath());
+        } catch(IOException e) {
+            LOG.log(Level.SEVERE, "Could not save task manager state. Scheduled tasks not saved on shutdown!", e);
         }
     }
 }
